1fe96724089c966aa8cf52a0ffdc8aeb3f4abc58
Gard Inge Rosvold <Gard-Inge.Rosvold@ffi.no>
2017-08-04 09:05:11 +0200

    Some checking of url for NASA authenticity and the target is an image implemented. Also updated readme with more information.

--- NASARSSIR/src/NASARSSIR.java
+++ NASARSSIR/src/NASARSSIR.java
@@ -17,7 +17,7 @@ import javax.imageio.ImageIO;
 /**
  * Created by Gard on 03/08/2017.
  * @author Gard
- * @version 0.1
+ * @version 0.5
  */
 public class NASARSSIR {
 
@@ -31,7 +31,10 @@ public class NASARSSIR {
             List entries = feed.getEntries();
             for ( Object listItem : entries ) {
                 if ( listItem instanceof SyndEntryImpl ) {
-                    checkEntry( (SyndEntryImpl) listItem, args[0] );
+                    if ( !checkEntry( (SyndEntryImpl) listItem, args[0] ) ) {
+                        System.out.println("Image already saved. Aborting...");
+                        System.exit(0);
+                    }
                 }
             }
 
@@ -42,22 +45,58 @@ public class NASARSSIR {
         }
     }
 
-    private static void checkEntry(SyndEntryImpl entry, String path) throws IOException {
+    /**
+     * Checks if an Syndicate Entry implementation has an enclosed list of image urls,
+     * @param entry the entry to check
+     * @param path path to save image if entry is valid
+     * @return a boolean value representing all images saved or _one_ failed
+     * @throws IOException
+     */
+    private static boolean checkEntry(SyndEntryImpl entry, String path) throws IOException {
         for ( Object listItem : entry.getEnclosures()) {
             if ( listItem instanceof SyndEnclosureImpl ) {
                 SyndEnclosureImpl enclosure = (SyndEnclosureImpl) listItem;
                 String url = enclosure.getUrl().replace("http://", "https://");
                 String filename = url.substring(url.lastIndexOf("/") + 1);
-                saveImageTo(url, path, filename);
+                if ( isNasaImage(enclosure.getUrl()) ) {
+                    if ( !saveImageTo(url, path, filename) ) {
+                        return false;
+                    }
+                    System.out.format("Saved '%s' to '%s'\n", filename, path);
+                }
             }
         }
+        return true;
     }
 
-    private static void saveImageTo(String url, String absolutePath, String filename) throws IOException {
+    /**
+     * Checks if a given url is of an image from 'http://www.nasa.gov/sites/default/files/thumbnails/image/'
+     * @param url the url to check
+     * @return boolean value representing if it is an image from given source
+     */
+    private static boolean isNasaImage(String url) {
+        return url.startsWith("http://www.nasa.gov/sites/default/files/thumbnails/image/") &&
+                (url.endsWith(".jpg") || url.endsWith(".jpeg") || url.endsWith("png") );
+    }
+
+    /**
+     * Saves image at url to the absolute path with given filename.
+     * @param url the url to an image
+     * @param absolutePath absolute-path-to the folder to store in
+     * @param filename name of the new image
+     * @return a boolean value representing if the image were successfully stored
+     * @throws IOException
+     */
+    private static boolean saveImageTo(String url, String absolutePath, String filename) throws IOException {
+        // Creates path as 'absolutePath/filename' by checking if absolutePath included char '/' at the end.
+        String path = String.format("%s%s", absolutePath.charAt(absolutePath.length()-1) == '/' ? absolutePath : absolutePath + "/", filename);
+        File outFile = new File(path);
+        if ( outFile.exists() ) {
+            return false;
+        }
         URL website = new URL(url);
-        File outFile = new File(String.format("%s%s", absolutePath.charAt(absolutePath.length()-1) == '/' ? absolutePath : absolutePath + "/", filename));
         BufferedImage img = ImageIO.read(website);
-        ImageIO.write(img, "jpg", outFile);
+        return ImageIO.write(img, "jpg", outFile);
     }
 
 }
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # NASARSSIR
-NASARSSImageReader
+**NASARSSI**mage**R**eader checks the NASA daily image feed for new images, and stores them to given directory.
+
+Stops if any image already exists, starting from last because of the RSS feed build.
+
+###*Security of link/url is* ***your*** *responsibility*
+
+## Usage
+
+```
+ java NASARSSIR PATH
+ where:
+ ~PATH  -  is the (preferred absolute) path to where you want to store
+ ```
+
+## Build Dependencies
+
+*  rome-1.0 (https://rometools.github.io/rome/)
+*  jdom-1.1.3 (http://www.jdom.org/)
